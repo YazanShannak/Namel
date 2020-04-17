ccfa84ee754768f98cb0b87af84ae4653d872ebe
Yazan <yazansh7@hotmail.com>
2020-04-18 00:04:17 +0300

    Edit test variables

--- url_parser.py
+++ url_parser.py
@@ -5,12 +5,22 @@ from distributed_crawler.kafka.producer import Producer
 from distributed_crawler.crawler.url_parser import UrlParser
 
 hostname = os.environ.get("HOSTNAME")
-parser_name = os.environ.get("parser_name", hostname)
+# parser_name = os.environ.get("parser_name", hostname)
+parser_name = "Test Node"
 
 logger = Logger(name="url_parser")
-# consumer = Consumer(topic="domains", servers=["localhost:29092"], consumer_group="url_parsers", managed=True)
-# producer = Producer(topic="urls", node_name=parser_name, servers=["localhost:29092"])
+consumer = Consumer(topic="domains", servers=["localhost:29092"], consumer_group="url_parsers", managed=True)
+producer = Producer(topic="urls", node_name=parser_name, servers=["localhost:29092"])
 
-parser = UrlParser(domain="http://books.toscrape.com")
-parser.parsed_urls.subscribe(on_next=lambda x: print(x))
-parser.crawl_all()
+
+def produce_url(url):
+    logger.log(url)
+    producer.send_message({"url": url})
+
+
+while True:
+    message = consumer.get_message()
+    domain = message['message']['url']
+    parser = UrlParser(domain=domain)
+    parser.parsed_urls.subscribe(on_next=produce_url)
+    parser.crawl_all()
