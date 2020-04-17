260d190bb5de1afaa49fd1527d904065128039b6
Yazan <yazansh7@hotmail.com>
2020-04-18 01:20:49 +0300

    Filter external Links

--- distributed_crawler/crawler/url_parser.py
+++ distributed_crawler/crawler/url_parser.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit
 import requests
 from rx.subject import Subject
 
@@ -7,6 +7,7 @@ from rx.subject import Subject
 class UrlParser:
     def __init__(self, domain: str):
         self.domain = domain
+        self.domain_netloc = self.get_url_netloc(self.domain)
         self.parsed_urls = Subject()
         self.parsed_pages = []
 
@@ -14,11 +15,20 @@ class UrlParser:
     def get_page(url):
         return requests.get(url).content
 
+    @staticmethod
+    def get_url_netloc(url):
+        return urlsplit(url).netloc
+
     def parse_urls(self, url):
         page = self.get_page(url)
         soup = BeautifulSoup(page, features='html.parser')
-        return [a['href'] if a['href'].startswith("http") else urljoin(url, a['href']) for a in
+        urls = [a['href'] if a['href'].startswith("http") else urljoin(url, a['href']) for a in
                 soup.find_all("a", href=True)]
+        return self.filter_external_links(urls)
+
+    def filter_external_links(self, urls):
+        urls_netlocs = [(url, self.get_url_netloc(url)) for url in urls]
+        return [url for url, netloc in urls_netlocs if netloc == self.domain_netloc]
 
     def crawl_page(self, url):
         urls = self.parse_urls(url)
