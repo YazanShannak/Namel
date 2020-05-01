3cfc853ac415e7b9fe0726390b12ac7f9d5a2d73
Yazan <yazansh7@hotmail.com>
2020-05-01 22:21:16 +0300

    update and document run-parser.py

--- run-parser.py
+++ run-parser.py
@@ -3,7 +3,6 @@ from distributed_crawler.logs.logger import Logger
 from distributed_crawler.kafka.consumer import Consumer
 from distributed_crawler.kafka.producer import Producer
 from distributed_crawler.crawler.url_parser import UrlParser
-from distributed_crawler.crawler.entities import Domain
 
 hostname = os.environ.get("HOSTNAME")
 parser_name = os.environ.get("node_name", hostname)
@@ -14,15 +13,26 @@ consumer = Consumer(topic="domains", servers=servers, consumer_group="url_parser
 producer = Producer(topic="urls", node_name=parser_name, servers=servers)
 
 
-def produce_url(url):
-    logger.log(url)
-    producer.send_message(url)
+def produce_url(url_object):
+    """
+    Logs and produces a new url object to kafka topic "urls"
+    :param url_object: new url object to produce
+    :type url_object: Dictionary of url and required_data
+    """
+    logger.log(url_object)
+    producer.send_message(url_object)
 
 
 while True:
+    """
+    Infinitely get one message at a time from the kafka topic "domains",
+    instantiate a new UrlParser and crawls the urls, 
+    produces an url_object each time its pushed to the stream 
+    """
     message = consumer.get_message()
-    domain = Domain(message)
-    logger.log("Received domain {} to crawl".format(domain.url))
-    parser = UrlParser(domain=domain)
+    domain_url = message["url"]
+    domain_required_data = message["required_data"]
+    logger.log("Received domain {} to crawl".format(domain_url))
+    parser = UrlParser(domain_url=domain_url, required_data=domain_required_data)
     parser.parsed_urls.subscribe(on_next=produce_url)
-    parser.crawl_all()
+    parser.start_crawl()
