7d1fde700a85d681a56973b4b94b8266bb0ff37f
Yazan <yazansh7@hotmail.com>
2020-05-01 04:12:10 +0300

    Update api

--- api/app.py
+++ api/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from .resources.domain_job import DomainJob
+from pykafka import KafkaClient
+
+client = KafkaClient(hosts="localhost:29092")
+topics = client.topics
+domains = topics['domains']
+domains_producer = domains.get_producer()
 
 app = Flask(__name__)
 api = Api(app=app)
 
-api.add_resource(DomainJob, '/domains')
+api.add_resource(DomainJob, '/domains', resource_class_kwargs={"producer": domains_producer})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- api/resources/domain_job.py
+++ api/resources/domain_job.py
@@ -1,9 +1,23 @@
 from flask_restful import Resource
+from flask import request
+import json
 
 dummy_jobs = [
-    {"id": 1, "domain": "http://books.toscrape.com", "data": [{"key": "title", "xpath": "//h1[@class='title']"}]}]
+    {"id": 1, "domain": "http://books.toscrape.com", "data": [{"key": "title", "xpath": "//h1[@class='title']"}]}
+]
 
 
 class DomainJob(Resource):
+    def __init__(self, **kwargs):
+        self.producer = kwargs.get("producer")
+
     def get(self):
         return {"jobs": dummy_jobs}
+
+    def post(self):
+        payload = request.json
+        url, data = payload["url"], payload["data"]
+        obj = {"url": url, "data": data}
+        message = json.dumps(obj).encode()
+        self.producer.produce(message)
+        return obj
